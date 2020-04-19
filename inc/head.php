f852c8604cc266905538de6ca47f876c82655456
malyadev <malyadev248@gmail.com>
2020-04-19 17:59:20 +0200

    init repo

--- inc/head.php
+++ inc/head.php
@@ -4,6 +4,7 @@
     <meta charset="utf-8">
     <meta http-equiv="x-ua-compatible" content="ie=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="author" content="malyadev">
     <title>The Cookie Factory</title>
     <!-- Latest compiled and minified CSS -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"
